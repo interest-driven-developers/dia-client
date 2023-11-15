50ed7440cce548a045207fe14e0e5b21f860fc61
Cho Sung Bae <101175828+ChoBaeKR@users.noreply.github.com>
2023-11-16 00:46:19 +0900

    [fix]mock 세팅

--- client/src/mocks/handlers.ts
+++ client/src/mocks/handlers.ts
@@ -1,10 +1,11 @@
 import { rest } from "msw";
 
+function sleep(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 export const handlers = [
-  rest.get(
-    `${process.env.NEXT_PUBLIC_SERVER_URL}/articles/remainCnt`,
-    (req, res, ctx) => {
-      return res(ctx.json({ remain_cnt: 1 }));
-    }
-  ),
+  rest.get(`${process.env.NEXT_PUBLIC_SERVER_URL}/test`, (req, res, ctx) => {
+    return res(ctx.json({ remain_cnt: 1 }));
+  }),
 ];
