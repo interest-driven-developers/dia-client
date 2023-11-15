0756ef39ef3c2ac5aa28ca396bfbb2925927bb5d
Cho Sung Bae <101175828+ChoBaeKR@users.noreply.github.com>
2023-11-16 00:46:56 +0900

    [test]로그인 상황별 테스트 추가

--- client/src/app/solve/__tests__/index.test.tsx
+++ client/src/app/solve/__tests__/index.test.tsx
@@ -1,11 +1,24 @@
 import { render, screen } from "@testing-library/react";
 import Home from "../page";
+import { server } from "@/mocks/server";
+import { rest } from "msw";
 import "@testing-library/jest-dom";
 
+test("init page", () => {
+  render(<Home />);
+  // 스크립트 작성 버튼이 보이는지 확인
+  expect(screen.getByText("작성")).toBeInTheDocument();
+});
+// 로그인 X) 초기 풀이 화면에는 아무 스크립트도 없음
+test("메인 화면에 스크립트가 비어있을때 ", () => {
+  render(<Home />);
+  expect(screen.getByText("스크립트를 작성해보세요")).toBeInTheDocument();
+});
+// 로그인 O) 초기 풀이 화면에 '스크립트를 작성해보세요'라는 문구 출력
 // 로그인이 되어있지 않다면 '로그인 후 작성할 수 있습니다' 메세지 출력
 test("", () => {});
 
-// 로그인 후에도 아직 작성하지 않은 문제라면 '스크립트를 작성해보세요 메세지
+// 로그인 후에도 아직 작성하지 않은 문제라면 '스크립트를 작성해보세요' 메세지
 test("", () => {});
 
 // 스크립트에는 일단 텍스트 제외 내용은 작성 불가능함(추후 그림 업로드 추가)
