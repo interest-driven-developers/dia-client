70c4e57815214b00fa235b675d7f93c37f9fdb58
Cho Sung Bae <101175828+ChoBaeKR@users.noreply.github.com>
2023-11-15 17:11:44 +0900

    [feat]모의 면접 페이지

---
+++ client/src/app/mockinterview/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Image from "next/image";
+
+export default function Home() {
+
+  return (
+    <main className="h-screen flex flex-col justify-center items-center gap-y-7 max-w-3xl mx-auto">
+      모의 면접 페이지
+      
+    </main>
+  );
+}
