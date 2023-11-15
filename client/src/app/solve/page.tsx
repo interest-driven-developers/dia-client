c787d9614a5404e86ff75750bdb8f651ad540fe7
Cho Sung Bae <101175828+ChoBaeKR@users.noreply.github.com>
2023-11-15 17:05:58 +0900

    [feat] 문제 풀이 페이지

---
+++ client/src/app/solve/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Image from "next/image";
+
+export default function Home() {
+
+  return (
+    <main className="h-screen flex flex-col justify-center items-center gap-y-7 max-w-3xl mx-auto">
+      Problem solve 페이지
+      {/* 리스트 처리된 문제(스크립트)들이 나열되어있고, 스크립트 수정 기능까지 추가해야함 */}
+      {/*1. 로그인이 되어있지 않다면   */}
+    </main>
+  );
+}
