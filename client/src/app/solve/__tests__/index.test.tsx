feba623b63780676fc31167cb24fb9ff55e93f96
Cho Sung Bae <101175828+ChoBaeKR@users.noreply.github.com>
2023-11-16 01:18:09 +0900

    [test]msw 코드 추가

--- client/src/app/solve/__tests__/index.test.tsx
+++ client/src/app/solve/__tests__/index.test.tsx
@@ -1,13 +1,16 @@
 import { render, screen } from "@testing-library/react";
 import Home from "../page";
 import { server } from "@/mocks/server";
-import { rest } from "msw";
+import { http, HttpResponse } from "msw";
 import "@testing-library/jest-dom";
 
 test("init page", () => {
   render(<Home />);
   // 스크립트 작성 버튼이 보이는지 확인
   expect(screen.getByText("작성")).toBeInTheDocument();
+  // 스크립트가 비어있을때 '스크립트를 작성해보세요'라는 문구 출력
+  expect(screen.getByText("스크립트를 작성해보세요")).toBeInTheDocument();
+  // 비로그인시 세션 스토리지에 데이터 저장?
 });
 // 로그인 X) 초기 풀이 화면에는 아무 스크립트도 없음
 test("메인 화면에 스크립트가 비어있을때 ", () => {
@@ -23,3 +26,17 @@ test("", () => {});
 
 // 스크립트에는 일단 텍스트 제외 내용은 작성 불가능함(추후 그림 업로드 추가)
 test("", () => {});
+
+
+// msw test
+test("mock server test", async () => {
+    server.use(
+      http.get(`${process.env.NEXT_PUBLIC_SERVER_URL}/test`, () => {
+        return HttpResponse.json({ message: "hello there" });
+      })
+    );
+    render(<Home />);
+    const greeting = await screen.findByText("hello there");
+    expect(greeting).toBeInTheDocument();
+});
+    
--- client/src/mocks/handlers.ts
+++ client/src/mocks/handlers.ts
@@ -1,11 +1,11 @@
-import { rest } from "msw";
+import { http, HttpResponse } from "msw";
 
 function sleep(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 export const handlers = [
-  rest.get(`${process.env.NEXT_PUBLIC_SERVER_URL}/test`, (req, res, ctx) => {
-    return res(ctx.json({ remain_cnt: 1 }));
+  http.get(`${process.env.NEXT_PUBLIC_SERVER_URL}/test`, () => {
+    return HttpResponse.json({ message: "hello there" });
   }),
 ];
