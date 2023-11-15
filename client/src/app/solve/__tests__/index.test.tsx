f4e089c77522e4606b31ba45f85d4d6a5ed229af
Cho Sung Bae <101175828+ChoBaeKR@users.noreply.github.com>
2023-11-15 17:05:35 +0900

    [test]문제 상세 페이지 테스트코드 추가

---
+++ client/src/app/solve/__tests__/index.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from "@testing-library/react";
+import Home from "../page";
+import "@testing-library/jest-dom";
+
+// 로그인이 되어있지 않다면 '로그인 후 작성할 수 있습니다' 메세지 출력
+test("", () => {});
+
+// 로그인 후에도 아직 작성하지 않은 문제라면 '스크립트를 작성해보세요 메세지
+test("", () => {});
+
+// 스크립트에는 일단 텍스트 제외 내용은 작성 불가능함(추후 그림 업로드 추가)
+test("", () => {});
