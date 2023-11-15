d98815bfb816c8a04fceb855d354a04a17bc8e9b
Cho Sung Bae <101175828+ChoBaeKR@users.noreply.github.com>
2023-11-15 17:11:17 +0900

    [test]모의 면접 페이지 테스트 코드

---
+++ client/src/app/mockinterview/__tests__/index.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from "@testing-library/react";
+import Home from "../page";
+import "@testing-library/jest-dom";
+
+// 로그인 X) 연습은 할 수 있지만 히스토리가 남지 않음
+test("", () => {});
+
+// 로그인 X) 연습 후 '로그인하면 기록을 남길 수 있다'라는 메세지를 보여주어 가입 유도
+test("", () => {});
+
+// 엽습 후 내가 작성한 스크립트와 방금 말한 내용을 같이 보여주어 비교
+test("", () => {});
