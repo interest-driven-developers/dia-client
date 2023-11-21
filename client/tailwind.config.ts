44e73fe15576d6e1d5e5fb20ca708826c1cbcc89
Cho Sung Bae <101175828+ChoBaeKR@users.noreply.github.com>
2023-11-22 00:56:48 +0900

    [feat]flash animation

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -13,6 +13,16 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      keyframes: {
+        flash: {
+          '25%, 40%': { opacity: '0' },
+          '50%' : { opacity: '1'},
+          '75%' : { opacity: '0'},
+        }
+      },
+      animation: {
+        flash : 'flash 2s infinite',
+      }
     },
   },
   plugins: [],
