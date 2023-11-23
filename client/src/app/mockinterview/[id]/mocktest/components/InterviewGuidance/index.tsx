5453d912e344d2a1a34931cf9859a50108f264d5
Cho Sung Bae <101175828+ChoBaeKR@users.noreply.github.com>
2023-11-24 00:06:33 +0900

    [repactor]각 뷰별 컴포넌트 분리

---
+++ client/src/app/mockinterview/[id]/mocktest/components/InterviewGuidance/index.tsx
@@ -0,0 +1,85 @@
+"use client";
+import React, { useState } from "react";
+import Image from "next/image";
+import useSpeechToText, { ResultType } from "react-hook-speech-to-text";
+import { MicrophoneIcon } from "@heroicons/react/24/solid";
+interface InterViewGuidanceProps {
+  handleView: (view: number) => void;
+  handleResults: (results: string) => void;
+}
+
+export default function InterViewGuidance({
+  handleView, handleResults
+}: InterViewGuidanceProps) {
+  //   const [results, setResults] = useState<ResultType[]>([]);
+  const [isDone, setIsDone] = useState<boolean>(false);
+  const {
+    error,
+    interimResult,
+    setResults,
+    results,
+    isRecording,
+    startSpeechToText,
+    stopSpeechToText,
+  } = useSpeechToText({
+    continuous: true,
+    useLegacyResults: false,
+  });
+
+  if (error) return <p>Web Speech API is not available in this browser 🤷‍</p>;
+
+  const handleStop = () => {
+    setTimeout(() => {
+      stopSpeechToText();
+      handleView(1);
+      handleResults(interimResult!);
+      console.log(interimResult, results);
+    }, 1000);
+  };
+  return (
+    <>
+      <div className="mb-6">
+        <Image
+          src="/images/interviewer.jpeg"
+          alt="면접관 이미지"
+          width={600}
+          height={500}
+          className="rounded-md"
+        />
+      </div>
+
+      {/* 안내문 */}
+      <p className="text-lg text-center mb-8 text-gray-500">
+        버튼을 클릭하면 면접이 시작됩니다. <br />
+        차분한 마음으로 대기해주시고, <br />
+        {`"삐"`} 소리가 나오면 질문에 대한 답변을 시작해주세요.
+      </p>
+
+      <div>
+        <button
+          className={`mt-3 w-full ${
+            isRecording
+              ? "bg-red-500 hover:bg-red-700"
+              : "bg-indigo-500 hover:bg-indigo-700"
+          } text-white py-2 px-4 rounded-xl shadow-md focus:outline-none`}
+          onClick={isRecording ? handleStop : startSpeechToText}
+        >
+          {isRecording ? "녹음 종료 ✋" : "녹음 시작 🗣️"}
+        </button>
+
+        <div className={`flex justify-center mt-2 ${!isRecording && "hidden"}`}>
+          <MicrophoneIcon
+            className={"w-7 h-7 text-red-500 animate-flash"}
+          ></MicrophoneIcon>
+        </div>
+        <ul>
+          {/* {results.map((result) => (
+            <li key={result.timestamp}>{result.transcript}</li>
+          ))}
+          {interimResult && <li>{interimResult}</li>} */}
+        </ul>
+        {/* {isDone && <div>{interimResult && <li>{interimResult}</li>}</div>} */}
+      </div>
+    </>
+  );
+}
---
+++ client/src/app/mockinterview/[id]/mocktest/components/RecordingResults/index.tsx
@@ -0,0 +1,50 @@
+"use client";
+import { ResultType } from "react-hook-speech-to-text";
+interface RecordingResultsProps {
+  handleRestartRecording: () => void;
+  // handleSaveScript: () => void;
+  description: string;
+  transcripts: string;
+}
+
+export default function RecordingResults({
+  description,
+  transcripts,
+  handleRestartRecording,
+}: RecordingResultsProps) {
+  return (
+    <div className="w-full">
+      <div className="grid grid-cols-4 p-4 gap-2">
+        <div className="col-span-2">
+          <h2 className="text-xl font-sans mb-4">기존 스크립트</h2>
+          <div className="p-4 h-64 overflow-y-auto w-full bg-white rounded-lg shadow-md divide-y border-dashed border-2 border-indigo-500">
+            <p>{description}</p>
+          </div>
+        </div>
+        <div className="col-span-2">
+          <h2 className="text-xl font-sans mb-4">음성 인식 결과</h2>
+          <p className="text-xs text-gray-500 -mt-4">
+            * 음성인식의 정확도는 100%가 아닌점 참고해주세요{" "}
+          </p>
+          <div className="p-4 h-64 overflow-y-auto w-full bg-white rounded-lg shadow-md divide-y border-dashed border-2 border-emerald-500">
+            {<p> {transcripts}</p>}
+          </div>
+        </div>
+      </div>
+      <div className="mt-4 flex justify-end gap-2 mr-4">
+        <button
+          onClick={handleRestartRecording}
+          className="bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded-md focus:outline-none"
+        >
+          다시 녹음 🗣️
+        </button>
+        <button
+          // onClick={handleSaveScript}
+          className="bg-emerald-500 hover:bg-emerald-700 text-white font-bold py-2 px-4 rounded-md focus:outline-none"
+        >
+          결과 저장 💾
+        </button>
+      </div>
+    </div>
+  );
+}
