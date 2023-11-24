06e68324017a9fd59709ac6be1f1b18729426425
Cho Sung Bae <101175828+ChoBaeKR@users.noreply.github.com>
2023-11-24 22:47:12 +0900

    [feat]문제 리스트 서버 패칭 로직

---
+++ client/src/app/api/getQuestionList.ts
@@ -0,0 +1,15 @@
+import { cache } from "react";
+export const getQuestionList = cache(async () => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_SERVER_URL}/api/v0/interview/questions`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  const data = await res.json();
+  
+  return data.data.data;
+});
