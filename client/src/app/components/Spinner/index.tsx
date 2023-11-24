307656b76dfffd0e4a94d0122f6a5c37086bf645
Cho Sung Bae <101175828+ChoBaeKR@users.noreply.github.com>
2023-11-24 21:30:53 +0900

    [feat]로딩 스피너

---
+++ client/src/app/components/Spinner/index.tsx
@@ -0,0 +1,8 @@
+export default function Spinner() {
+  return (
+    <div className="flex justify-center">
+      <div className="w-10 h-10 rounded-full absolute border-2 border-solid border-gray-200"></div>
+      <div className="w-10 h-10 rounded-full animate-spin absolute border-2 border-solid border-indigo-500 border-t-transparent"></div>
+    </div>
+  );
+}
