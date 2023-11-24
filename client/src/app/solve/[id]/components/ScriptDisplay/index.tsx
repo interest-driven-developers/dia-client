918f23605e47e41eb62423863f61d8989a43bfbf
Cho Sung Bae <101175828+ChoBaeKR@users.noreply.github.com>
2023-11-24 21:32:43 +0900

    [feat]비로그인시 데이터 로컬스토리지 저장 로직

---
+++ client/src/app/solve/[id]/components/ScriptDisplay/index.tsx
@@ -0,0 +1,78 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import { CheckCircleIcon, XCircleIcon } from "@heroicons/react/24/solid";
+interface ScriptDisplayProps {
+  description: string;
+  setDescription: any;
+  isEditing: boolean;
+  setIsEditing: any;
+  handleSaveScript: any;
+  id: number;
+}
+import Spinner from "@/app/components/Spinner";
+export default function ScriptDisplay({
+  description,
+  setDescription,
+  isEditing,
+  setIsEditing,
+  handleSaveScript,
+  id,
+}: ScriptDisplayProps) {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  // 페이지 로딩 시, 로컬 스토리지에서 스크립트 불러오기
+  useEffect(() => {
+    const savedScript = localStorage.getItem(`${id}script`);
+    if (savedScript) {
+      setDescription(savedScript);
+    }
+    setIsLoading(false);
+  }, []);
+  return (
+    <>
+      <p className="text-xs font-sans text-gray-500 -mb-1.5">스크립트</p>
+      <div className="p-3 w-full mt-2 bg-white rounded-lg shadow-md divide-y border-dashed border-2 border-indigo-500 ">
+        {isLoading ? (
+          <div className="w-full h-12 flex justify-center justify-items-center mt-2 r-8">
+            <Spinner />
+          </div>
+        ) : isEditing ? (
+          <>
+            <textarea
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              className="w-full h-40 p-2 border rounded-md"
+            />
+            <div className="flex p-1 justify-end">
+              <XCircleIcon
+                onClick={() => setIsEditing(false)}
+                className="w-5 h-5 text-red-500 cursor-pointer hover:opacity-50"
+              ></XCircleIcon>
+              <CheckCircleIcon
+                onClick={() => handleSaveScript()}
+                className="w-5 h-5 text-indigo-500 cursor-pointer hover:opacity-50"
+              ></CheckCircleIcon>
+            </div>
+          </>
+        ) : (
+          <p className="whitespace-pre-wrap ">
+            {description ? (
+              <p>{description}</p>
+            ) : (
+              <div
+                onClick={() => setIsEditing(true)}
+                className="flex h justify-center cursor-pointer hover:opacity-50"
+              >
+                <p className="text-gray-500">
+                  스크립트가 작성되지 않았습니다. <br />
+                  지금 바로{" "}
+                  <span className="animate-pulse text-indigo-500">작성</span>
+                  해보세요✏️
+                </p>
+              </div>
+            )}
+          </p>
+        )}
+      </div>
+    </>
+  );
+}
--- client/src/app/solve/[id]/page.tsx
+++ client/src/app/solve/[id]/page.tsx
@@ -1,25 +1,28 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import {
   PencilIcon,
   MicrophoneIcon,
   CheckCircleIcon,
+  XCircleIcon,
 } from "@heroicons/react/24/solid";
+import ScriptDisplay from "./components/ScriptDisplay";
+import Spinner from "@/app/components/Spinner";
 const dummyData = {
   id: 1,
   title: "Solid 원칙에 대해 설명해보세요",
-  description:
-    "객체지향 설계에서 지켜줘야 할 5개의 소프트웨어 개발 원칙( SRP, OCP, LSP, ISP, DIP )을 말한다.",
+  description: "",
 };
 export default function Main({ params }: { params: { id: string } }) {
   const [description, setDescription] = useState<string>(dummyData.description);
   const [isEditing, setIsEditing] = useState<boolean>(false);
   // TODO : 로그인시 세션 저장
   let session = null;
+  // TODO 로그인시 이전 로컬 스토리지 데이터 불러오기
   const handleSaveScript = () => {
     // 스크립트 저장
-    // 스크립트 모달 닫기
+    localStorage.setItem(`${dummyData.id}script`, description);
     // 스크립트 리스트 업데이트
     setIsEditing(false);
   };
@@ -35,10 +38,10 @@ export default function Main({ params }: { params: { id: string } }) {
             </div>
           </Link>
           <div
-            className="p-1 rounded-lg hover:bg-gray-100"
+            className="p-1 rounded-lg hover:bg-gray-100 cursor-pointer"
             onClick={() => setIsEditing(true)}
           >
-            <PencilIcon className="w-5 h-5 text-indigo-400"></PencilIcon>
+            <PencilIcon className="w-5 h-5 text-indigo-500"></PencilIcon>
           </div>
         </div>
       </div>
@@ -47,35 +50,17 @@ export default function Main({ params }: { params: { id: string } }) {
           스크립트를 작성 및 모의 면접을 진행해보세요🧐
         </p>
       </div>
-      <p className="text-xs font-sans text-gray-500 -mb-1.5">스크립트</p>
-      <div className="p-3 w-full mt-2 bg-white rounded-lg shadow-md divide-y border-dashed border-2 border-indigo-500 ">
-        {isEditing ? (
-          <>
-            <textarea
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-              className="w-full h-40 p-2 border rounded-md"
-            />
-            <div className="flex p-1  justify-end ">
-              <CheckCircleIcon
-                onClick={() => handleSaveScript()}
-                className="w-5 h-5 text-indigo-500 cursor-pointer hover:opacity-50"
-              ></CheckCircleIcon>
-            </div>
-          </>
-        ) : (
-          <p className="whitespace-pre-wrap">
-            {description ? (
-              <p>{description}</p>
-            ) : (
-              <p>스크립트가 없습니다. 스크립트를 작성해주세요✏️</p>
-            )}
-          </p>
-        )}
-      </div>
+      <ScriptDisplay
+        id={dummyData.id}
+        description={description}
+        isEditing={isEditing}
+        setDescription={setDescription}
+        setIsEditing={setIsEditing}
+        handleSaveScript={handleSaveScript}
+      ></ScriptDisplay>
       {!session && (
         <p className="text-sm font-sans text-gray-500">
-          *로그인 후 이용해주시면 스크립트가 영구히 저장됩니다 😊
+          *로그인 후 이용해주시면 스크립트가 영구히 저장됩니다 💾
         </p>
       )}
       <Link href={`/mockinterview/${params.id}/mocktest`}>
