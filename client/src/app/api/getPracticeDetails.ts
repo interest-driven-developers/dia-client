330c362a79eb7e48ef46669485b09790b0a53232
Cho Sung Bae <101175828+ChoBaeKR@users.noreply.github.com>
2023-11-25 12:53:01 +0900

    [feat]모의면접 시 문제 디테일 서버 로직

---
+++ client/src/app/api/getPracticeDetails.ts
@@ -0,0 +1,15 @@
+import { cache } from "react";
+export const getPracticeDetails = cache(async (id: string | null | undefined) => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_SERVER_URL}/api/v0/interview/practice/single?interviewQuestionPkValue=${id}`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  const data = await res.json();
+
+  return data.data;
+});
