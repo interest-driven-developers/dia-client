7384e639135dd1833df6fc236863509a61e9cb96
Cho Sung Bae <101175828+ChoBaeKR@users.noreply.github.com>
2023-12-02 02:18:29 +0900

    [fix] 데이터 서버패칭 로직 수정

--- client/src/app/solve/[id]/page.tsx
+++ client/src/app/solve/[id]/page.tsx
@@ -1,6 +1,7 @@
 import { getQuestionDetails } from "@/app/api/getQuestionDetails";
 import QuestionContainer from "./components/QuestionContainer";
 import { Metadata } from "next";
+export const dynamic = "force-dynamic";
 
 export const generateMetadata = async ({ params }: any): Promise<Metadata> => {
   const data = await getQuestionDetails(params.id);
@@ -14,10 +15,11 @@ export default async function Main({ params }: { params: { id: string } }) {
   const result = await getQuestionDetails(params.id);
   // TODO : 로그인시 세션 저장
   let session = null;
+  console.log(result);
   // TODO 로그인시 이전 로컬 스토리지 데이터 불러오기
   return (
     <main className="h-screen max-w-3xl mx-auto">
-      <QuestionContainer {...result} session={session}></QuestionContainer>
+      <QuestionContainer {...result.data} session={session}></QuestionContainer>
     </main>
   );
 }
