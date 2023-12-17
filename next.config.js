9a1672d46a99c9ee90967c8d1b860bb8e026cb55
Cho Sung Bae <101175828+ChoBaeKR@users.noreply.github.com>
2023-12-18 00:50:58 +0900

    [chore]env 설정

--- next.config.js
+++ next.config.js
@@ -9,6 +9,14 @@ const nextConfig = {
       },
     ],
   },
+  env: {
+    GITHUB_ID: process.env.GITHUB_ID,
+    GITHUB_SECRET: process.env.GITHUB_SECRET,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    AWS_RDS_URI: process.env.AWS_RDS_URI,
+    NEXT_PUBLIC_SERVER_URL: process.env.NEXT_PUBLIC_SERVER_URL,
+    NEXT_PUBLIC_CLOUDFRONT_URL: process.env.NEXT_PUBLIC_CLOUDFRONT_URL,
+  },
 };
-
-module.exports = nextConfig
+module.exports = nextConfig;
