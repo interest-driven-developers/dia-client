c539657153ac89cb1b4310a7cf14b689a3035f5a
Cho Sung Bae <101175828+ChoBaeKR@users.noreply.github.com>
2023-12-17 22:31:58 +0900

    [test] 컴포넌트 수정

--- src/app/signIn/page.tsx
+++ src/app/signIn/page.tsx
@@ -1,6 +1,6 @@
 import { getProviders, signIn } from "next-auth/react";
 import { getServerSession } from "next-auth/next";
-import Login from "./components/Login/page";
+import Login from "./components/Login";
 export default async function SignInPage() {
 
   const providers = await getProviders();
