ce62290d216abca7edb82aa016a712289a80320a
Cho Sung Bae <101175828+ChoBaeKR@users.noreply.github.com>
2023-12-20 05:57:21 +0900

    [fix] 깃허브 아이콘 수정

--- src/app/signIn/page.tsx
+++ src/app/signIn/page.tsx
@@ -1,5 +1,4 @@
-import { getProviders, signIn } from "next-auth/react";
-import { getServerSession } from "next-auth/next";
+import { getProviders } from "next-auth/react";
 import Login from "./components/Login";
 export default async function Page() {
   const providers = await getProviders();
