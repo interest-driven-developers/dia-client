d0c8bb3351647899b1730d2563d55cc472b52f3f
Cho Sung Bae <101175828+ChoBaeKR@users.noreply.github.com>
2024-01-04 02:06:54 +0900

    [feat] custom primary color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,30 +1,33 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       keyframes: {
         flash: {
-          '25%, 40%': { opacity: '0' },
-          '50%' : { opacity: '1'},
-          '75%' : { opacity: '0'},
-        }
+          "25%, 40%": { opacity: "0" },
+          "50%": { opacity: "1" },
+          "75%": { opacity: "0" },
+        },
       },
       animation: {
-        flash : 'flash 2s infinite',
-      }
+        flash: "flash 2s infinite",
+      },
+      colors: {
+        primary: "#6366f1",
+      },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
