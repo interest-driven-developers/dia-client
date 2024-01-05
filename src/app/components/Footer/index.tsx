dde0d634acc6478df0899fe584beefc23dd09423
Cho Sung Bae <101175828+ChoBaeKR@users.noreply.github.com>
2024-01-06 00:05:46 +0900

    [fix]mobile footer ui

--- src/app/components/Footer/index.tsx
+++ src/app/components/Footer/index.tsx
@@ -3,7 +3,7 @@ import GithubIcon from "@/app/ui/GithubIcon";
 export default function Footer() {
   return (
     <footer className="flex items-center justify-between p-6 bg-gray-100">
-      <div className="flex gap-2 sm:gap-4">
+      <div className="flex gap-1 sm:gap-4">
         <Link
           className="text-xs sm:text-sm text-gray-500 hover:underline"
           href="#"
@@ -24,7 +24,7 @@ export default function Footer() {
         </Link>
       </div>
       <div className="flex gap-1 sm:gap-3">
-        <div className="mt-1 flex space-x-2 text-xs sm:text-sm text-gray-500">
+        <div className="mt-1 flex space-x-1  sm:space-x-2 text-xs sm:text-sm text-gray-500">
           <span>DIA</span>
           <div>{` • `}</div>
           <div>{`© ${new Date().getFullYear()}`}</div>
