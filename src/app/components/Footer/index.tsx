dff3aebe47b05a6ecf04698947b671539996ab40
Cho Sung Bae <101175828+ChoBaeKR@users.noreply.github.com>
2024-01-05 23:57:35 +0900

    [fix] 모바일 ui

--- src/app/components/Footer/index.tsx
+++ src/app/components/Footer/index.tsx
@@ -2,26 +2,6 @@ import Link from "next/link";
 import GithubIcon from "@/app/ui/GithubIcon";
 export default function Footer() {
   return (
-    // <footer className="bg-gray-100">
-    //   <div className="mt-16 flex flex-col items-center">
-    //     <div className="mb-3 flex space-x-4"></div>
-    //     <div className="mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
-    //       <span>DIA</span>
-    //       <div>{` • `}</div>
-    //       <div>{`© ${new Date().getFullYear()}`}</div>
-    //       <div>{` • `}</div>
-    //       <Link
-    //         className="hover:underline"
-    //         href="https://github.com/interest-driven-developers"
-    //       >
-    //         Github
-    //       </Link>
-    //     </div>
-    //     <div className="mb-8 flex text-sm text-gray-500 dark:text-gray-400 ">
-    //       <p>Beta Version</p>
-    //     </div>
-    //   </div>
-    // </footer>
     <footer className="flex items-center justify-between p-6 bg-gray-100">
       <div className="flex gap-2 sm:gap-4">
         <Link
