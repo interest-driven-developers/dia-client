82b9cd89b5f06853131d588bd06d23a50795d7fb
Cho Sung Bae <101175828+ChoBaeKR@users.noreply.github.com>
2024-01-07 21:42:48 +0900

    [fix]mobile header fixed

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -40,7 +40,7 @@ export default function Header({ session }: HeaderProps) {
     setIsMenuOpen(false);
   };
   return (
-    <header className="bg-white shadow-sm w-screen sm:mx-auto">
+    <header className="fixed z-40 sm:relative bg-white shadow-sm w-screen sm:mx-auto">
       <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 sm:w-1/2">
         <div className="flex items-center justify-between h-16">
           {/* 로고 */}
