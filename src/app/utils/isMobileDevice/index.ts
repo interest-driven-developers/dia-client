7cd09515b8c9d5ad3fca000326678f7f64d906b7
Cho Sung Bae <101175828+ChoBaeKR@users.noreply.github.com>
2024-01-08 00:00:40 +0900

    [feat]mobile mobile environment detection func

---
+++ src/app/utils/isMobileDevice/index.ts
@@ -0,0 +1,9 @@
+// User Agent 문자열을 가져옴
+const userAgent = navigator.userAgent;
+
+// User Agent 문자열에 모바일 특정 키워드가 포함되는지 확인
+export default function isMobileDevice() {
+  // 예시: "Android", "iPhone", "iPad" 등의 키워드로 모바일 여부 확인
+  return /Android|iPhone|iPad|iPod|BlackBerry|Windows Phone/i.test(userAgent);
+}
+
