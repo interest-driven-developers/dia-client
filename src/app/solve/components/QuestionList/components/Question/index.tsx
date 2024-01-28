61a380f29bddf4ee48bc784f2f2953ae9db86201
Cho Sung Bae <101175828+ChoBaeKR@users.noreply.github.com>
2024-01-29 01:51:41 +0900

    [fix]font-height

--- src/app/solve/components/QuestionList/components/Question/index.tsx
+++ src/app/solve/components/QuestionList/components/Question/index.tsx
@@ -17,14 +17,19 @@ export default function Question({ id, title, description }: QuestionProps) {
     <Link href={`/solve/${id}`}>
       <div className="flex flex-col bg-[#FAFAFA] rounded-lg px-5 py-4 gap-5 cursor-pointer hover:opacity-70 dark:bg-indigo-950 dark:border-indigo-800">
         <div>
-          <h1 className="text-[#BDBDBD] text-[12px] sm:text-lg">Question</h1>
-          <h2 className="text-[#212121] text-[18px] sm:text-2xl font-semibold dark:text-slate-100">
+          <h1 className="text-[#BDBDBD] text-[12px] sm:text-lg font-semibold leading-3">
+            Question
+          </h1>
+          <h2 className="text-[#212121] text-[18px] sm:text-2xl font-bold leading-5 dark:text-slate-100">
             {title}
           </h2>
         </div>
-        <p className="text-[#616161] text-[12px] sm:text-lg mt-2">
-          👥 <span className="text-[#651FFF]">{Math.floor(Math.random() * 2000) + 1}</span>명의 사용자가
-          도전했습니다
+        <p className="text-[#616161] text-[12px] sm:text-lg mt-2 font-semibold leading-3">
+          👥{" "}
+          <span className="text-[#651FFF]">
+            {Math.floor(Math.random() * 2000) + 1}
+          </span>
+          명의 사용자가 도전했습니다
         </p>
       </div>
     </Link>
