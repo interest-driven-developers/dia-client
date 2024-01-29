1f1d4233e34ffd33efb6fedb649505cd1ce1f012
Cho Sung Bae <101175828+ChoBaeKR@users.noreply.github.com>
2024-01-30 02:19:56 +0900

    [feat]Add Icons

---
+++ src/app/ui/EqualizerIcon/index.tsx
@@ -0,0 +1,124 @@
+export default function EqualizerIcon(props: any) {
+  return (
+    <svg
+      width="360"
+      height="177"
+      viewBox="0 0 360 177"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className="animate-pulse flex w-100 max-w-screen-xl mx-auto"
+    >
+      <rect x="14" y="72" width="11" height="33" fill="#7C4DFF" />
+      <rect x="-6" y="72" width="11" height="33" fill="#7C4DFF" />
+      <rect x="36" y="66" width="10" height="45" fill="#7C4DFF" />
+      <rect
+        x="326"
+        y="111"
+        width="10"
+        height="45"
+        transform="rotate(-180 326 111)"
+        fill="#7C4DFF"
+      />
+      <rect
+        x="345"
+        y="105"
+        width="10"
+        height="33"
+        transform="rotate(-180 345 105)"
+        fill="#7C4DFF"
+      />
+      <rect
+        x="364"
+        y="105"
+        width="10"
+        height="33"
+        transform="rotate(-180 364 105)"
+        fill="#7C4DFF"
+      />
+      <rect x="57" y="43" width="11" height="91" fill="#7C4DFF" />
+      <rect
+        x="305"
+        y="134"
+        width="11"
+        height="91"
+        transform="rotate(-180 305 134)"
+        fill="#7C4DFF"
+      />
+      <rect x="79" y="26" width="10" height="125" fill="#7C4DFF" />
+      <rect
+        x="283"
+        y="151"
+        width="10"
+        height="125"
+        transform="rotate(-180 283 151)"
+        fill="#7C4DFF"
+      />
+      <rect x="122" y="26" width="11" height="125" fill="#7C4DFF" />
+      <rect x="143" y="37" width="11" height="103" fill="#7C4DFF" />
+      <rect x="166" y="53" width="9" height="79" fill="#7C4DFF" />
+      <rect x="186" y="40" width="11" height="97" fill="#7C4DFF" />
+      <rect x="208" y="10" width="11" height="157" fill="#7C4DFF" />
+      <rect
+        x="240"
+        y="151"
+        width="11"
+        height="125"
+        transform="rotate(-180 240 151)"
+        fill="#7C4DFF"
+      />
+      <rect x="100" y="10" width="11" height="157" fill="#7C4DFF" />
+      <rect
+        x="262"
+        y="177"
+        width="11"
+        height="177"
+        transform="rotate(-180 262 177)"
+        fill="#7C4DFF"
+      />
+      <g filter="url(#filter0_d_53_663)">
+        <circle cx="180" cy="93" r="46" fill="#F8F3FF" />
+      </g>
+      <path
+        d="M181 99.7895C184.794 99.7895 187.834 96.6863 187.834 92.8421L187.857 78.9474C187.857 75.1032 184.794 72 181 72C177.206 72 174.143 75.1032 174.143 78.9474V92.8421C174.143 96.6863 177.206 99.7895 181 99.7895ZM193.114 92.8421C193.114 99.7895 187.309 104.653 181 104.653C174.691 104.653 168.886 99.7895 168.886 92.8421H165C165 100.739 171.217 107.269 178.714 108.404V116H183.286V108.404C190.783 107.293 197 100.762 197 92.8421H193.114Z"
+        fill="#7C4DFF"
+      />
+      <defs>
+        <filter
+          id="filter0_d_53_663"
+          x="130"
+          y="47"
+          width="100"
+          height="100"
+          filterUnits="userSpaceOnUse"
+          color-interpolation-filters="sRGB"
+        >
+          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="4" />
+          <feGaussianBlur stdDeviation="2" />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow_53_663"
+          />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect1_dropShadow_53_663"
+            result="shape"
+          />
+        </filter>
+      </defs>
+    </svg>
+  );
+}
