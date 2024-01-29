d183ad556953170ae9fb3347e67e7154079fc1bc
Cho Sung Bae <101175828+ChoBaeKR@users.noreply.github.com>
2024-01-30 01:12:01 +0900

    [chore]Del Darkmode UI

--- src/app/components/Header/Components/ToggleMenu/index.tsx
+++ src/app/components/Header/Components/ToggleMenu/index.tsx
@@ -9,21 +9,21 @@ type ToggleMenuProps = {
 export default function ToggleMenu(props: ToggleMenuProps) {
   return (
     <div
-      className={`absolute right-1 w-2/3 h-96 z-50 bg-slate-100 shadow-lg rounded-md p-6 block md:hidden dark:bg-slate-800 ${props.animationClass}`}
+      className={`absolute right-1 w-2/3 h-96 z-50 bg-white shadow-lg rounded-md p-6 block md:hidden  ${props.animationClass}`}
     >
       <span className="items-center mb-5 text-3xl font-bold custom-color cursor-pointer">
         DIA
       </span>
       <nav className="grid gap-2">
         <Link
-          className="flex w-full items-center py-2 text-lg font-semibold text-slate-600 dark:text-slate-100"
+          className="flex w-full items-center py-2 text-lg font-semibold text-slate-600 "
           href="/"
           onClick={props.onClick}
         >
           홈
         </Link>
         <Link
-          className="flex w-full items-center py-2 text-lg font-semibold text-slate-600 dark:text-slate-100"
+          className="flex w-full items-center py-2 text-lg font-semibold text-slate-600 "
           href="/solve"
           onClick={props.onClick}
         >
