1f6536c76bab9e03fe7787db3b3e0fdf11c0cd2f
Cho Sung Bae <101175828+ChoBaeKR@users.noreply.github.com>
2024-02-07 16:35:09 +0900

    [chore]Del UndeclaredCode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
-import React from "react";
-import Image from "next/image";
-import Link from "next/link";
-import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
 import Intro from "./components/Intro";
-export default async function Home() {
 
+export default async function Home() {
   // let session = await getServerSession(authOptions);
   return (
     <main className="flex flex-col justify-center mx-auto items-center sm:px-6 py-16 sm:w-1/2">
