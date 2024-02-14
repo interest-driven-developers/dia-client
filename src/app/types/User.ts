330801aaf8238562ea90514f4b3055beaa388096
Cho Sung Bae <101175828+ChoBaeKR@users.noreply.github.com>
2024-02-14 20:05:18 +0900

    [chore]User 타입 수정

--- src/app/types/User.ts
+++ src/app/types/User.ts
@@ -1,6 +1,6 @@
 export interface User {
   nickname?: string | null | undefined;
   email?: string | null | undefined;
-  image?: string | null | undefined;
+  image_url?: string | null | undefined;
   pk?: string | number | null | undefined;
 }
