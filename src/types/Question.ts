f6e35a203e208e23eb06e84f663077f53a73bce3
Cho Sung Bae <101175828+ChoBaeKR@users.noreply.github.com>
2024-02-17 02:51:11 +0900

    [type]질문 타입 수정

--- src/types/Question.ts
+++ src/types/Question.ts
@@ -1,6 +1,7 @@
+import type { VoiceType } from "./Voice";
 export interface Question {
   pkValue: number;
   korTitleValue: string;
-  categories: {}
-  voices: {}
+  categories: {};
+  voices: VoiceType;
 }
