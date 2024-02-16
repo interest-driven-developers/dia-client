1388de573096f1bdb1e4bb5a9d554772459e4cc8
Cho Sung Bae <101175828+ChoBaeKR@users.noreply.github.com>
2024-02-17 02:50:50 +0900

    [type]연습 결과 타입 추가

---
+++ src/types/PracticeResult.ts
@@ -0,0 +1,7 @@
+export type PracticeResult = {
+  interviewQuestionPkValue: number;
+  contentValue: string;
+  typeValue: "SINGLE" | "MULTI";
+  elapsedTimeValue: number;
+  filePathValue: string | null;
+};
