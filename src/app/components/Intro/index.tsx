3c0cd8a5b013e893fad7a27a8314ab9e8127fe37
Cho Sung Bae <101175828+ChoBaeKR@users.noreply.github.com>
2024-02-19 00:06:09 +0900

    [fix]인트로 페이지 UI 수정

--- src/app/components/Intro/index.tsx
+++ src/app/components/Intro/index.tsx
@@ -25,8 +25,8 @@ export default function Intro() {
       </section>
 
       <section className="w-full py-12 md:py-24 lg:py-32">
-        <div className="container px-4 md:px-6">
-          <div className="grid items-center gap-6 lg:grid-cols-[1fr_500px] lg:gap-12 xl:grid-cols-[1fr_550px]">
+        <div className="container px-4 ">
+          <div className="grid items-center gap-6 lg:grid-cols-2 lg:gap-12 xl:grid-cols-2">
             <Image
               alt="Voice Recognition"
               className="mx-auto h-auto w-full aspect-video overflow-hidden rounded-xl object-cover object-center sm:w-full lg:order-last"
@@ -51,8 +51,8 @@ export default function Intro() {
         </div>
       </section>
       <section className="w-full py-12 md:py-24 lg:py-32">
-        <div className="container px-4 md:px-6">
-          <div className="grid items-center gap-6 lg:grid-cols-[1fr_500px] lg:gap-12 xl:grid-cols-[1fr_550px]">
+        <div className="container px-4">
+          <div className="grid items-center gap-6 lg:grid-cols-2 lg:gap-12 xl:grid-cols-2">
             <Image
               alt="Text-to-Speech"
               className="mx-auto h-auto w-full  aspect-video overflow-hidden rounded-xl object-cover object-center sm:w-full"
@@ -77,7 +77,7 @@ export default function Intro() {
       </section>
       <section className="w-full py-12 md:py-24 lg:py-32">
         <div className="container px-4 md:px-6">
-          <div className="grid items-center gap-6 lg:grid-cols-[1fr_500px] lg:gap-12 xl:grid-cols-[1fr_550px]">
+          <div className="grid items-center gap-6 lg:grid-cols-2 lg:gap-12 xl:grid-cols-2">
             <Image
               alt="Mock Interview"
               className="mx-auto w-full h-auto aspect-video overflow-hidden rounded-xl object-cover object-center sm:w-full lg:order-last"
