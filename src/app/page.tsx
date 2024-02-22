15d592a8a5549b059b615cd1fa3a5bc8146c522d
Cho Sung Bae <101175828+ChoBaeKR@users.noreply.github.com>
2024-02-22 21:26:10 +0900

    [fix]스크롤바 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Intro from "./components/Intro";
 export default async function Home() {
   // let session = await getServerSession(authOptions);
   return (
-    <main className="flex flex-col justify-center mx-auto items-center sm:px-6 py-16 sm:w-1/2">
+    <main className="flex flex-col justify-center mx-auto items-center sm:px-6 py-16 sm:w-1/2 no-scrollbar">
       <Intro />
     </main>
   );
