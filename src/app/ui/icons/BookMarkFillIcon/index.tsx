5d27ccbedbd91bb908a843d57b6d1fb5657c3cb0
Cho Sung Bae <101175828+ChoBaeKR@users.noreply.github.com>
2024-02-27 02:03:49 +0900

    [feat]아이콘 추가

--- src/app/ui/icons/BookMarkFillIcon/index.tsx
+++ src/app/ui/icons/BookMarkFillIcon/index.tsx
@@ -2,7 +2,7 @@ type Props = {
   className?: string;
   onClick?: () => void;
 };
-export default function BookMarkIcon({ className, onClick}: Props) {
+export default function BookMarkFillIcon({ className, onClick }: Props) {
   return (
     <svg
       width="14"
@@ -10,11 +10,11 @@ export default function BookMarkIcon({ className, onClick}: Props) {
       viewBox="0 0 14 18"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
+      className="group-hover:opacity-80"
       onClick={onClick}
-      className={className}
     >
       <path
-        d="M12 0H2C0.9 0 0.0100002 0.9 0.0100002 2L0 18L7 15L14 18V2C14 0.9 13.1 0 12 0ZM12 15L7 12.82L2 15V2H12V15Z"
+        d="M12 0H2C0.9 0 0.0100002 0.9 0.0100002 2L0 18L7 15L14 18V2C14 0.9 13.1 0 12 0Z"
         fill="#591FD9"
       />
     </svg>
