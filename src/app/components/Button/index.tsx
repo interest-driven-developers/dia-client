2e8eb1876a93c89e08cc70f6f02147ae3bc79b2a
Cho Sung Bae <101175828+ChoBaeKR@users.noreply.github.com>
2024-03-19 18:44:40 +0900

    [fix]버튼 UI 수정

--- src/app/components/Button/index.tsx
+++ src/app/components/Button/index.tsx
@@ -9,7 +9,7 @@ export default function Button({onClick, className, children}: ButtonProps) {
   return (
     <button
       className={twMerge(
-        "flex justify-center w-full px-3 py-3 bg-primary-600 rounded-[5px] items-center hover:opacity-90 whitespace-nowrap text-white font-bold text-[16px] leading-[19.2px] sm:text-xl",
+        "flex justify-center w-full px-3 py-[13px] bg-primary-600 rounded-[5px] items-center hover:opacity-90 whitespace-nowrap text-white font-bold text-[16px] leading-[19.2px] sm:text-xl",
         className
       )}
       onClick={onClick}
